24ad3868163e1a32a965d22cb56c89642c47799b
aivyngo <aivyngo@gmail.com>
2025-07-20 19:40:42 +0000

    remove test from tracking

--- apcsaagent-test/test_agent.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "http://192.222.54.121:8060/generate"
-data = {
-    "prompt": "Generate a free response question on recursion"
-}
-
-response = requests.post(url, json=data)
-print(response.json())
